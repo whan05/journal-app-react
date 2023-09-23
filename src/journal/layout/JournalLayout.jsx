7237585a3d092eab7ca895d9e2ba63e2948cf878
Whanderley Fonseca <wfonsecap-m@ucenfotec.ac.cr>
2023-09-23 08:52:05 -0600

    Add new feature: Finished the UI view

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,31 +1,24 @@
-import { Box } from "@mui/material"
+import { Box, Toolbar } from "@mui/material";
+import { Navbar, SideBar } from "../components";
 
+const drawerWidth = 240;
 
-export const JournalLayout = ({children}) => {
+export const JournalLayout = ({ children }) => {
   return (
-    <Box 
-        sx={{display: "flex"}}
-    >
+    <Box sx={{ display: "flex" }}>
+      <Navbar drawerWidth={drawerWidth} />
+      <SideBar drawerWidth={drawerWidth}/>
 
-        {/* Navbar */}
-
-
-        {/* Sidebar */}
-
-
-        <Box 
-            component='main'
-            sx={{
-                flexGrow: 1,
-                p: 3
-            }}
-        >
-
-                {/* Toolbar */}
-                {children}
-        </Box>
-
-
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+        }}
+      >
+        <Toolbar/>
+        {children}
+      </Box>
     </Box>
-  )
-}
+  );
+};
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,14 +1,32 @@
-import { Typography } from "@mui/material"
-import { JournalLayout } from "../layout/JournalLayout"
+import { IconButton } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
+import { NoteView, NothingSelectedView } from "../views";
+import { AddOutlined } from "@mui/icons-material";
 
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam veniam laboriosam incidunt, non eius consequatur alias, minus iste sunt id eos sequi explicabo neque tenetur commodi eaque minima hic maxime!</Typography>
-
       {/* Nothing Selected */}
+      <NothingSelectedView />
       {/* Note View */}
+      {/* <NoteView/> */}
+      <IconButton
+        size="large"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': {
+            backgroundColor: 'error.main',
+            opacity: 0.9
+          },
+          position: "fixed",
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30}}/>
+      </IconButton>
     </JournalLayout>
-  )
-}
+  );
+};
